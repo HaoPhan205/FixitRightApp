49e59c0d2f0b27af8f48258e184ad178c5067c73
Rous1141 <132892586+Rous1141@users.noreply.github.com>
2025-03-11 15:23:06 +0700

    Tabs

--- app/login/home/_layout.tsx
+++ app/login/home/_layout.tsx
@@ -1,17 +1,41 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { Tabs } from 'expo-router'
+import {styles} from '@/style/tabs'
+import { Ionicons } from "@expo/vector-icons";
 
 export default function _layout() {
   return (
    <Tabs
+      screenOptions={{
+        headerShown:false,
+        tabBarStyle:styles.tabBar,
+        tabBarActiveTintColor: "#E1F2EC", // light blue
+        tabBarInactiveTintColor: "#3C4D70", // default
+      }}
    >
      <Tabs.Screen name="index" 
-         options={{ tabBarLabel: 'Home' }}
+         options={{ tabBarLabel: 'Home',
+          tabBarIcon: ({ color }) => <Ionicons name="home" size={28} color={"white"} />,
+
+         }}
+     />
+      <Tabs.Screen name="bookmark" 
+         options={{ tabBarLabel: 'Bookmark',
+          tabBarIcon: ({ color }) => <Ionicons name="bookmark" size={24}  color={"white"} />,
+          }}
+     />
+     <Tabs.Screen name="chat" 
+         options={{ tabBarLabel: 'Chat',
+          tabBarIcon: ({ color }) => <Ionicons name="chatbox-ellipses" size={24}  color={"white"} />,
+          }}
      />
      <Tabs.Screen name="profile" 
-         options={{ tabBarLabel: 'Profile' }}
+         options={{ tabBarLabel: 'Profile',
+          tabBarIcon: ({ color }) => <Ionicons name="person-circle-sharp" size={24}  color={"white"} />,
+          }}
      />
+
    </Tabs>
   )
 }
---
+++ style/tabs.ts
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    tabBar: {
+        backgroundColor: "#3C4D70", // Dark Blue
+        height: "10%",
+        borderTopLeftRadius: 20,
+        borderTopRightRadius: 20,
+        position: "absolute",
+        bottom: 0,
+        left: 0,
+        right: 0,
+        borderTopWidth: 0, // Remove top border line
+      },
+})
